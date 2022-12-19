c67b3a3fd3d939c8771509d95d76367f810df631
Pedro Takahashi <pedroh.takahashi@gmail.com>
2022-12-19 20:54:26 -0300

    initial project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Body from './Componets/Body';
+import Footer from './Componets/Footer';
+import Header from './Componets/Header';
+const Main = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return(
+  <>
+    <Header/>
+    <Body />
+    <Footer />
+  </>
+)
 }
 
-export default App;
+export default Main;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Main } from "./Components/Pages";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Main from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
